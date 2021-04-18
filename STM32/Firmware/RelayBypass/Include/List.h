81bfcc463911651d47cc73b9ae0b71f4e991b535
Alexander Zaytsev <yup.disconnected.yup@gmail.com>
2021-04-19 00:36:44 +0300

    Add minimal functions for List

---
+++ STM32/Firmware/RelayBypass/Include/List.h
@@ -0,0 +1,25 @@
+// List.h
+#ifndef LIST_HEADER_FILE_H
+#define LIST_HEADER_FILE_H
+
+#include "Common.h"
+
+typedef struct
+{
+	char state;
+	char channel;
+	char specificator;
+} StateStruct;
+
+typedef struct Node 
+{
+    StateStruct cmdBlock;
+    struct Node *pNext;
+} Node;
+
+void List_Push(Node** ppHead, StateStruct command);
+StateStruct List_Pop(Node** ppHead);
+Node* List_GetLast(Node* pHead);
+void List_PushBack(Node* pHead, StateStruct command);
+
+#endif

c6d1e4ce24d5334a03a5749f1d3a2f9000b9d0b5
Alexander Zaytsev <yup.disconnected.yup@gmail.com>
2021-04-30 13:06:30 +0300

    Change btn irq to falling edge

--- STM32/Firmware/RelayBypass/Core/Src/gpio.c
+++ STM32/Firmware/RelayBypass/Core/Src/gpio.c
@@ -71,7 +71,7 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = B_BTN_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B_BTN_GPIO_Port, &GPIO_InitStruct);
 
@@ -84,7 +84,7 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = A_BTN_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(A_BTN_GPIO_Port, &GPIO_InitStruct);
 

f7245fcdcd9b24867289b8321bd8654377907b9a
Alexander Zaytsev <yup.disconnected.yup@gmail.com>
2021-05-09 14:22:01 +0300

    Add sdcard lib

---
+++ STM32/Firmware/MicroSdTest/sdcard/sdcard.h
@@ -0,0 +1,59 @@
+/*
+MIT License
+
+Copyright (c) 2018 Aleksander Alekseev
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/* vim: set ai et ts=4 sw=4: */
+#ifndef __SDCARD_H__
+#define __SDCARD_H__
+
+#include "stm32f4xx_hal.h"
+
+#define SDCARD_SPI_PORT      hspi1
+#define SDCARD_CS_Pin        GPIO_PIN_5 // Arduino shield: D4
+#define SDCARD_CS_GPIO_Port  GPIOB
+
+extern SPI_HandleTypeDef SDCARD_SPI_PORT;
+
+// call before initializing any SPI devices
+void SDCARD_Unselect();
+
+// all procedures return 0 on success, < 0 on failure
+
+int SDCARD_Init();
+int SDCARD_GetBlocksNumber(uint32_t* num);
+int SDCARD_ReadSingleBlock(uint32_t blockNum, uint8_t* buff); // sizeof(buff) == 512!
+int SDCARD_WriteSingleBlock(uint32_t blockNum, const uint8_t* buff); // sizeof(buff) == 512!
+
+// Read Multiple Blocks
+int SDCARD_ReadBegin(uint32_t blockNum);
+int SDCARD_ReadData(uint8_t* buff); // sizeof(buff) == 512!
+int SDCARD_ReadEnd();
+
+// Write Multiple Blocks
+int SDCARD_WriteBegin(uint32_t blockNum);
+int SDCARD_WriteData(const uint8_t* buff); // sizeof(buff) == 512!
+int SDCARD_WriteEnd();
+
+// TODO: read lock flag? CMD13, SEND_STATUS
+
+#endif // __SDCARD_H__
